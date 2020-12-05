a2b116ce1d6ecbeb835fd2f726f601321e446fdf
bkRahul <rahulsatya1068@gmail.com>
2020-12-06 00:03:52 +0530

    added lodash to perform deep comparison of projects

---
+++ src/hooks/useProjects/projects.jsx
@@ -0,0 +1,28 @@
+import { isEqual } from "lodash";
+import { useEffect, useState } from "react";
+
+import { firebase } from "../../firebase";
+
+export const useProjects = () => {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    firebase
+      .firestore()
+      .collection("projects")
+      .where("userId", "==", "chtjuMWL3bEWyMN")
+      .orderBy("projectId")
+      .get()
+      .then((snapshot) => {
+        const allProjects = snapshot.docs.map((project) => ({
+          ...project.data(),
+          docId: project.id,
+        }));
+
+        //need to check to avoid infinite loop
+        !isEqual(allProjects, projects) && setProjects(allProjects);
+      });
+  }, [projects]);
+
+  return { projects, setProjects };
+};
