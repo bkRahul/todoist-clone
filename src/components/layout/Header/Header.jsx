981d99eb03028b90b36ff67acb02367b2a7a59ed
bkRahul <rahulsatya1068@gmail.com>
2020-12-10 00:31:59 +0530

    added darkmode and quick add task modal

--- src/components/layout/Header/Header.jsx
+++ src/components/layout/Header/Header.jsx
@@ -1,8 +1,12 @@
-import React from "react";
-import { FaPizzaSlice } from "react-icons/fa";
-import logo from "../../../assets/images/logo.png";
+import React, { useState } from 'react';
+import { FaPizzaSlice } from 'react-icons/fa';
+import logo from '../../../assets/images/logo.png';
+import { AddTask } from '../../Tasks/AddTask/AddTask';
 
-export const Header = () => {
+export const Header = ({ darkMode, setDarkMode }) => {
+  const [showMain, setShowMain] = useState(false);
+  const [showQuickAddTask, setShowQuickAddTask] = useState(false);
+
   return (
     <div>
       <header className="header" data-testid="header">
@@ -12,15 +16,32 @@ export const Header = () => {
           </div>
           <div className="settings">
             <ul>
-              <li data-testid="quick-add-task-action" className="settings__add">
+              <li
+                data-testid="quick-add-task-action"
+                className="settings__add"
+                onClick={() => {
+                  setShowMain(true);
+                  setShowQuickAddTask(true);
+                }}
+              >
                 +
               </li>
-              <li data-testid="dark-mode-action" className="settings__darkmode">
+              <li
+                data-testid="dark-mode-action"
+                className="settings__darkmode"
+                onClick={() => setDarkMode(!darkMode)}
+              >
                 <FaPizzaSlice />
               </li>
             </ul>
           </div>
         </nav>
+        <AddTask
+          showAddTaskMain={false}
+          shouldShowMain={showMain}
+          showQuickAddTask={showQuickAddTask}
+          setShowQuickAddTask={setShowQuickAddTask}
+        />
       </header>
     </div>
   );
